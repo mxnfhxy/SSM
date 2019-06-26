68ef6e3335a1841f3e5befcb6e512c6c17c163a0
mxnfhxy <mxnfhxy@163.com>
2019-06-27 00:47:14 +0800

    first commit

---
+++ IdeaProjects/demo/src/main/java/com/example/demo/dao/HappinessDao.java
@@ -0,0 +1,16 @@
+package com.example.demo.dao;
+
+import com.example.demo.domain.Happiness;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface HappinessDao {
+    @Select("SELECT * FROM happiness WHERE city = #{city}")
+    Happiness findHappinessByCity(@Param("city") String city);
+
+    @Insert("INSERT INTO happiness(city, num) VALUES(#{city}, #{num})")
+    int insertHappiness(@Param("city") String city, @Param("num") Integer num);
+}
---
+++ IdeaProjects/demo/src/main/java/com/example/demo/domain/Happiness.java
@@ -0,0 +1,7 @@
+package com.example.demo.domain;
+
+public class Happiness {
+    private Long id;
+    private String city;
+    private Integer num;
+}
